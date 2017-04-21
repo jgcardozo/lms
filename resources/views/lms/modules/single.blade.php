0727fd4ac01521826cb3f18e0f9c3eea1e2d2a10
object505 <pbeluent@gmail.com>
2017-04-21 17:14:21 +0200

    Fix for lesson complete

--- resources/views/lms/modules/single.blade.php
+++ resources/views/lms/modules/single.blade.php
@@ -75,7 +75,7 @@
                                         <div class="course-progress course-progress--completed">Completed <span class="course-progress__bar course-progress__bar--completed"></span></div>
                                     @elseif($lesson->is_locked)
                                         @if($lesson->is_date_locked)
-                                            <div class="course-progress" data-date=" until {{ date('d-m-Y', strtotime($lesson->lock_date)) }}">
+                                            <div class="course-progress course-progress__lesson" data-date=" until {{ date('d-m-Y', strtotime($lesson->lock_date)) }}">
                                                 Unlocks {{ date('d-m-Y', strtotime($lesson->lock_date)) }} <span class="course-progress__bar course-progress__bar--locked"></span>
                                             </div>
                                         @else
