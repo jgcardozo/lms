5baf3670340a713de4835678b5134a2ee48a6477
codeartmk <contact@codeart.mk>
2017-05-17 17:01:15 +0200

    Fix payment alerts.

--- resources/views/lms/alerts/alert.blade.php
+++ resources/views/lms/alerts/alert.blade.php
@@ -1,6 +1,8 @@
-@foreach($askAlert as $_askAlert)
-    <div class="ask-alert ask-alert--full ask-alert--full--{{ $_askAlert['status'] }}" data-key="{{ route('alert.view', $_askAlert['key']) }}">
-        {!! $_askAlert['message'] !!}
-        <a href="#" class="js-close-ask-alert"></a>
-    </div>
-@endforeach
+@if(!empty($askAlert))
+    @foreach($askAlert as $_askAlert)
+        <div class="ask-alert ask-alert--full ask-alert--full--{{ $_askAlert['status'] }}" data-key="{{ route('alert.view', $_askAlert['key']) }}">
+            {!! $_askAlert['message'] !!}
+            <a href="#" class="js-close-ask-alert"></a>
+        </div>
+    @endforeach
+@endif
