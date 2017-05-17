8d856092ae8555352f07bce1f820c59846ea7407
codeartmk <contact@codeart.mk>
2017-05-17 15:48:33 +0200

    Payment alerts. Refactoring

--- resources/views/lms/alerts/alert.blade.php
+++ resources/views/lms/alerts/alert.blade.php
@@ -1,4 +1,6 @@
-<div class="ask-alert ask-alert--full ask-alert--full--{{ $askAlert['status'] }}" data-key="{{ route('alert.view', $askAlert['key']) }}">
-    {!! $askAlert['message'] !!}
-    <a href="#" class="js-close-ask-alert"></a>
-</div>
+@foreach($askAlert as $_askAlert)
+    <div class="ask-alert ask-alert--full ask-alert--full--{{ $_askAlert['status'] }}" data-key="{{ route('alert.view', $_askAlert['key']) }}">
+        {!! $_askAlert['message'] !!}
+        <a href="#" class="js-close-ask-alert"></a>
+    </div>
+@endforeach
