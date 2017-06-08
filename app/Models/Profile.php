2275e6a9c2545867e009b1a9b902394788608aa0
codeartmk <contact@codeart.mk>
2017-06-08 10:27:35 +0200

    Edit profile admin panel. Added feature for manual marking sessions as complete

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -9,6 +9,10 @@ class Profile extends Model
 	protected $primaryKey = 'user_id';
 	protected $guarded = 'user_id';
 
+	protected $fillable = [
+		'first_name', 'last_name', 'phone1', 'company'
+	];
+
     public function user()
 	{
 		return $this->belongsTo('App\Models\User');
