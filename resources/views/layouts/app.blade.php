61e779df3254d39d4eb88639af69f6eafa5da444
Codeart Network <codeartmk@users.noreply.github.com>
2017-12-05 18:20:05 +0100

    removed helpscout script

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -92,16 +92,6 @@
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}"></script>
 
-        <script>!function(e,o,n){window.HSCW=o,window.HS=n,n.beacon=n.beacon||{};var t=n.beacon;t.userConfig={},t.readyQueue=[],t.config=function(e){this.userConfig=e},t.ready=function(e){this.readyQueue.push(e)},o.config={docs:{enabled:!1,baseUrl:""},contact:{enabled:!0,formId:"0a256aba-28cc-11e7-9841-0ab63ef01522"}};var r=e.getElementsByTagName("script")[0],c=e.createElement("script");c.type="text/javascript",c.async=!0,c.src="https://djtflbt20bdde.cloudfront.net/",r.parentNode.insertBefore(c,r)}(document,window.HSCW||{},window.HS||{});</script>
-        <script>
-            HS.beacon.ready(function() {
-                HS.beacon.identify({
-                    name: '{{ Auth::user()->name }}',
-                    email: '{{ Auth::user()->email }}'
-                });
-            });
-        </script>
-
         <script>
             mixpanel.identify({{ \Auth::user()->id }});
             mixpanel.people.set({
