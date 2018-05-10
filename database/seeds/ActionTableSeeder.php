9fdf323a5588907ea0d2c5cfa97ec4a5efa61c20
Ivan Janev <developer@codeart.mk>
2018-05-10 16:23:49 +0200

    Created required tables for the custom activiy monitor

---
+++ database/seeds/ActionTableSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ActionTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    private $actions = [
+        'started',
+        'completed',
+        'clicked',
+        'changed',
+        'updated',
+        'logged in',
+        'posted to',
+        'downloaded',
+        'passed',
+        'passed (retaken)',
+        'failed',
+        'failed (retaken)'
+    ];
+
+    public function run()
+    {
+        foreach ($this->actions as $action) {
+        DB::table('actions')
+            ->insert([
+                'name' => $action,
+                'created_at' => now(),
+                'updated_at' => now()
+            ]);
+        }
+    }
+}
---
+++ database/seeds/ActivityTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ActivityTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    private $activities = [
+        'Facebook',
+        'Survey',
+        'Billing details',
+        'Password',
+        'Contact details',
+        'Quiz'
+    ];
+
+    public function run()
+    {
+        foreach ($this->activities as $activity) {
+            DB::table('activities')
+                ->insert([
+                    'name' => $activity,
+                    'created_at' => now(),
+                    'updated_at' => now()
+                ]);
+        }
+    }
+}
