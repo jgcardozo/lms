f2f354bd2f5c237289ccad75426cd5f19b9f7464
Ivan Janev <developer@codeart.mk>
2018-05-21 14:29:48 +0200

    updated seeder

--- database/seeds/CohortCleanupSeeder.php
+++ database/seeds/CohortCleanupSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class CohortCleanupSeeder extends Seeder
 {
@@ -11,13 +12,17 @@ class CohortCleanupSeeder extends Seeder
      */
     public function run()
     {
-        $users = \App\Models\User::all()->chunk('100');
-        foreach ($users as $user) {
-            foreach ($user as $u) {
-                if (! $u->hasTag(3786)) {
-                    $u->cohorts()->detach();
-                }
-            }
+        $userWithTags = DB::table('tag_user')
+            ->select()
+            ->where('tag_id','=',3786)
+            ->get()->pluck('user_id')->toArray();
+        $users = \App\Models\User::all()->pluck('id')->toArray();
+
+        $userIds = array_diff($users,$userWithTags);
+
+        foreach ($userIds as $id) {
+            $user = \App\Models\User::find($id);
+            $user->cohorts()->detach();
         }
     }
 }
