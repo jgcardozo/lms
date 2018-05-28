9dbd6258908b2c11df474502cb0df980c7674cbc
Ivan Janev <developer@codeart.mk>
2018-05-28 12:58:03 +0200

    Fixed bug rollbar:#83 Illuminate\Database\QueryException: SQLSTATE[23000] and #98 Illuminate\Database\QueryException: SQLSTATE[23000]

---
+++ app/Http/Requests/Admin/UserStoreCrudRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+class UserStoreCrudRequest extends \Backpack\CRUD\app\Http\Requests\CrudRequest
+{
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return \Auth::check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'email'    => 'required|unique:'.config('laravel-permission.table_names.users', 'users').',email',
+            'name'     => 'required',
+            'password' => 'required|confirmed',
+            'phone1'   => 'required',
+            'company'  => 'required'
+        ];
+    }
+}
