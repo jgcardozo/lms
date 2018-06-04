f1cb050eab6aed314826e5b4432711a2dfb9b9e8
Ivan Janev <developer@codeart.mk>
2018-06-04 14:22:12 +0200

    Created a controller for the NotificationLog and added a delete route to the function

---
+++ app/Http/Controllers/NotificationLogController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\NotificationLog;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class NotificationLogController extends Controller
+{
+    public function delete($id)
+    {
+        $notificationLog = NotificationLog::find($id);
+        $uuid = $notificationLog->uuid;
+        $notifications = DB::table('notifications')
+            ->where('data->uuid',$uuid)
+            ->delete();
+
+        $notificationLog->delete();
+
+        return redirect()->back();
+    }
+}
