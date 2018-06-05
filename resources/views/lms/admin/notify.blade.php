734774de0b29c288b7e8e88b200e46563a3fb0b2
Ivan Janev <developer@codeart.mk>
2018-06-05 19:00:49 +0200

    Changed the notify blade to a table and made a separate view for userDetails to clean up code

--- resources/views/lms/admin/notify.blade.php
+++ resources/views/lms/admin/notify.blade.php
@@ -75,54 +75,73 @@
                     <div class="box-title">Logs of notifications sent</div>
                 </div>
                 <div class="box-body">
-                    @foreach($logs as $log)
-                        <p style="display: inline-block"><b>{{ $log->user->name }}</b> sent a notification to
-                            <b>
-                                @if(is_array($log->subject))
-                                    {{$log->subject['type']}}
-                                    @if($log->subject['type'] !== "All users")
-                                        <button type="button" class="btn btn-info" data-toggle="modal" data-target="#detailsModal{{$log->id}}">View Details</button>
-                                    @endif
-                                @else
-                                    {{ $log->subject }}
-                                @endif
-                            </b>
-                            with a message <b>{{ strip_tags($log->message) }}</b></p>
-                        <form method="post" action="{{ route('notification.log.delete',$log->id) }}">
-                            {{ method_field('delete') }}
-                            {{ csrf_field() }}
-                            <button class="btn btn-primary" value="{{ $log->id  }}">Delete</button>
-                        </form>
-                        <hr>
-
-                        <!-- Modal -->
-                        <div id="detailsModal{{$log->id}}" class="modal fade" role="dialog">
-                            <div class="modal-dialog">
-
-                                <!-- Modal content-->
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                        <h4 class="modal-title">Details</h4>
-                                    </div>
-                                    <div class="modal-body">
-                                        @if($log->subject['type'] === "cohortCourse")
-                                            @include('lms.notifications.partials.courseDetails')
-                                        @elseif($log->subject['type'] === "specificUsers")
-                                            @foreach($log->subject['users'] as $user)
-                                                <p>{{ $user->name }} - {{ $user->email }}</p>
-                                                <hr>
-                                            @endforeach
+                    <table class="table" id="logTable" style="overflow-y: scroll">
+                        @foreach($logs as $log)
+                            <tr>
+                                <td>
+                                    <p style="display: inline-block"><b>{{ $log->user->name }}</b> sent a notification to
+                                        <b>
+                                            @if(is_array($log->subject))
+                                                @if($log->subject['type'] === "specificUsers")
+                                                    Specific Users
+                                                @elseif($log->subject['type'] === "cohortCourse")
+                                                    Cohort/Course
+                                                @else
+                                                    All Users
+                                                @endif
+                                            @else
+                                                {{ $log->subject }}
+                                            @endif
+                                        </b>
+                                        with a message <b>{{ strip_tags($log->message) }}</b>
+                                    </p>
+                                </td>
+                                <td>
+                                    <form method="post" action="{{ route('notification.log.delete',$log->id) }}">
+                                        {{ method_field('delete') }}
+                                        {{ csrf_field() }}
+                                        <button class="btn btn-primary" value="{{ $log->id  }}">Delete</button>
+                                    </form>
+                                </td>
+                                <td>
+                                    @if(is_array($log->subject))
+                                        @if($log->subject['type'] !== "All users")
+                                            <button type="button" class="btn btn-info" data-toggle="modal" data-target="#detailsModal{{$log->id}}">View Details</button>
                                         @endif
-                                    </div>
-                                    <div class="modal-footer">
-                                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                    @else
+                                        {{ $log->subject }}
+                                    @endif
+                                </td>
+
+                                <!-- Modal -->
+                                <div id="detailsModal{{$log->id}}" class="modal fade" role="dialog">
+                                    <div class="modal-dialog">
+
+                                        <!-- Modal content-->
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                                <h4 class="modal-title">Details</h4>
+                                            </div>
+                                            <div class="modal-body">
+                                                @if($log->subject['type'] === "cohortCourse")
+                                                    @include('lms.notifications.partials.courseDetails')
+                                                @elseif($log->subject['type'] === "specificUsers")
+                                                    @include('lms.notifications.partials.usersDetails')
+                                                @endif
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                            </div>
+                                        </div>
+
                                     </div>
                                 </div>
+                            </tr>
+                        @endforeach
+                    </table>
 
-                            </div>
-                        </div>
-                    @endforeach
+                    {{$logs->fragment('logTable')->links()}}
                 </div>
             </div>
         </div>
