e8c89d63f24f88407f32a40dab6ee7d64e5aa2e0
Ivan Janev <developer@codeart.mk>
2018-06-06 11:07:38 +0200

    Change title of popup

--- resources/views/lms/admin/notify.blade.php
+++ resources/views/lms/admin/notify.blade.php
@@ -116,7 +116,7 @@
                                         <div class="modal-content">
                                             <div class="modal-header">
                                                 <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                                <h4 class="modal-title">Details</h4>
+                                                <h2 class="modal-title">Sent to:</h2>
                                             </div>
                                             <div class="modal-body">
                                                 @if($log->subject['type'] === "cohortCourse")
