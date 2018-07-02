4517028924c8b739f280fd3bfd1ae4bed051516d
Developer Codeart <developer@codeart.mk>
2018-07-02 14:37:44 +0200

    updated the dashboard blade and js to dinamically update the charts and to have a loading overlay

--- resources/assets/js/dashboard.js
+++ resources/assets/js/dashboard.js
@@ -2,10 +2,17 @@ $(document).ready(function () {
     //chart Declaration
     Chart.defaults.global.legend.position = 'bottom';
 
-    var firstTime = true;
+    var firstTimeModule = true;
+    var firstTimeLesson = true;
+    var firstTimeSession = true;
 
     var inputFields = [];
     var colorPallete = [];
+
+    var modulesChart = '';
+    var lessonChart = '';
+    var sessionChart = '';
+
 
     $.ajax({
         url: "dashboard/fields",
@@ -27,16 +34,21 @@ $(document).ready(function () {
     });
 
     $('#course_id').on('change',function () {
+        addBlurAndLoading();
         fillCohorts($('#course_id :selected').val());
         fillModules($('#course_id :selected').val());
+        $('#lesson_id').empty();
+        $('#lesson_id').append("<option value='' > Select a lesson </option>");
         getPieChartsData();
     });
 
     $('#cohort_id').on('change',function () {
+        addBlurAndLoading();
         getPieChartsData();
     });
 
     $('#module_id').on('change',function () {
+        addBlurAndLoading();
         if($('#module_id :selected').val() !== "") {
             fillLessons($('#module_id :selected').val());
         } else {
@@ -47,6 +59,7 @@ $(document).ready(function () {
     });
 
     $('#lesson_id').on('change',function () {
+        addBlurAndLoading();
         getPieChartsData();
     });
 
@@ -123,9 +136,11 @@ $(document).ready(function () {
 
                     window.piedata = response;
 
-
                     createPieCharts(response);
                 }
+
+                $('#overlay_loading').css('display','none');
+                $('#blurDiv').css('filter','');
             },
             error: function error(_error) {
                 console.log(_error);
@@ -135,7 +150,14 @@ $(document).ready(function () {
 
     function createPieCharts(data) {
 
+        if(!firstTimeModule) {
+            modulesChart.data.labels = Object.keys(data[0]);
+            modulesChart.data.datasets[0].data = Object.values(data[0]);
+            modulesChart.update();
 
+            generateModuleLegend(data);
+
+        } else {
             var ctx = $('#modules');
             modulesChart = new Chart(ctx, {
                 type: 'pie',
@@ -154,25 +176,27 @@ $(document).ready(function () {
                     },
                     legend: {
                         display: false
-                    },
-                    legendCallback: function (chart) {
-                        var html = "<ul style='list-style: none; font-size: 24px'>";
-                        Object.keys(data[0]).forEach(function (module) {
-                            html += "<li>"+module+" "+data[6][module]+"% - Avg "+data[3][module]+" days</li>";
-                        });
-                        html += "</ul>";
-                        return html;
                     }
                 }
             });
 
-            $('#moduleLegend').html(modulesChart.generateLegend());
+            generateModuleLegend(data);
 
-            if(data[1].length !== 0) {
+            firstTimeModule = false;
+        }
+
+        if(data[1].length !== 0) {
+            if(!firstTimeLesson) {
+                lessonChart.data.labels = Object.keys(data[1]);
+                lessonChart.data.datasets[0].data = Object.values(data[1]);
+                lessonChart.update();
+
+                generateLessonLegend(data);
+            } else {
                 $('#lessons, #lessonLegend').css('display','inherit');
 
                 var ctx1 = $('#lessons');
-                var lessonChart = new Chart(ctx1, {
+                lessonChart = new Chart(ctx1, {
                     type: 'pie',
                     data: {
                         labels: Object.keys(data[1]),
@@ -189,28 +213,29 @@ $(document).ready(function () {
                         },
                         legend: {
                             display: false
-                        },
-                        legendCallback: function (chart) {
-                            var html = "<ul style='list-style: none; font-size: 24px'>";
-                            Object.keys(data[1]).forEach(function (lesson) {
-                                html += "<li>"+lesson+" "+data[7][lesson]+"% - Avg "+data[4][lesson]+" days</li>";
-                            });
-                            html += "</ul>";
-                            return html;
                         }
                     }
                 });
+                generateLessonLegend(data);
 
-                $('#lessonLegend').html(lessonChart.generateLegend());
-            } else {
-                $('#lessons, #lessonLegend').css('display','none');
+                firstTimeLesson = false;
             }
+        } else {
+            $('#lessons, #lessonLegend').css('display','none');
+        }
 
-            if(data[2].length !== 0) {
+        if(data[2].length !== 0) {
+            if(!firstTimeSession){
+                sessionChart.data.labels = Object.keys(data[2]);
+                sessionChart.data.datasets[0].data = Object.values(data[2]);
+                sessionChart.update();
+
+                generateSessionLegend(data);
+            } else {
                 $('#sessions, #sessionLegend').css('display','inherit');
 
                 var ctx3 = $('#sessions');
-                var sessionChart = new Chart(ctx3, {
+                sessionChart = new Chart(ctx3, {
                     type: 'pie',
                     data: {
                         labels: Object.keys(data[2]),
@@ -227,24 +252,55 @@ $(document).ready(function () {
                         },
                         legend: {
                             display: false
-                        },
-                        legendCallback: function (chart) {
-                            var html = "<ul style='list-style: none; font-size: 24px'>";
-                            Object.keys(data[2]).forEach(function (session) {
-                                html += "<li>"+session+" "+data[8][session]+"% </li>";
-                            });
-                            html += "</ul>";
-                            return html;
                         }
                     }
                 });
 
-                $('#sessionLegend').html(sessionChart.generateLegend());
-            } else {
-                $('#sessions, #sessionLegend').css('display','none');
+                generateSessionLegend(data);
+
+                firstTimeSession = false;
             }
+        } else {
+            $('#sessions, #sessionLegend').css('display','none');
+        }
 
 
+
+    }
+
+    function generateModuleLegend(data) {
+        var html = "<ul style='list-style: none; font-size: 24px'>";
+        Object.keys(data[0]).forEach(function (module) {
+            html += "<li>"+module+" "+data[6][module]+"% - Avg "+data[3][module]+" days</li>";
+        });
+        html += "</ul>";
+
+        $('#moduleLegend').html(html);
+    }
+
+    function generateLessonLegend(data) {
+        var html = "<ul style='list-style: none; font-size: 24px'>";
+        Object.keys(data[1]).forEach(function (lesson) {
+            html += "<li>"+lesson+" "+data[7][lesson]+"% - Avg "+data[4][lesson]+" days</li>";
+        });
+        html += "</ul>";
+
+        $('#lessonLegend').html(html);
+    }
+
+    function generateSessionLegend(data) {
+        var html = "<ul style='list-style: none; font-size: 24px'>";
+        Object.keys(data[2]).forEach(function (session) {
+            html += "<li>"+session+" "+data[8][session]+"% </li>";
+        });
+        html += "</ul>";
+
+        $('#sessionLegend').html(html);
+    }
+
+    function addBlurAndLoading() {
+        $('#overlay_loading').css('display','inherit');
+        $('#blurDiv').css('filter','blur(3px)');
     }
 
 });
