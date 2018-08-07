1621e024907080a3aa589d5c56182092fc572747
Developer Codeart <developer@codeart.mk>
2018-08-07 10:33:44 +0200

    wip

--- database/seeds/CacheSeeder.php
+++ database/seeds/CacheSeeder.php
@@ -249,18 +249,22 @@ INNER JOIN (SELECT created_at  as en,user_id FROM `progresses` WHERE (progress_t
 
         foreach ($module_id->lmsLessons as $lesson) {
 
-            $lessonCount[$lesson->title] = DB::select("select COUNT(*) as counted from progresses where progress_type LIKE '%Lesson' AND progress_id=? AND user_id IN (" . implode(",", $users) . ")", [$lesson->id]);
-            $lessonCount[$lesson->title] = $lessonCount[$lesson->title][0]->counted;
-            $lessonCount[$lesson->title] = $this->percentage($totalUsers, $lessonCount[$lesson->title]);
+            if($lesson->sessions->count() > 0) {
+                $lessonCount[$lesson->title] = DB::select("select COUNT(*) as counted from progresses where progress_type LIKE '%Lesson' AND progress_id=? AND user_id IN (" . implode(",", $users) . ")", [$lesson->id]);
+                $lessonCount[$lesson->title] = $lessonCount[$lesson->title][0]->counted;
+                $lessonCount[$lesson->title] = $this->percentage($totalUsers, $lessonCount[$lesson->title]);
 
-            $sessionId = $lesson->sessions->first()->id;
+                $sessionId = $lesson->sessions->first()->id;
 
-            $query = DB::select("SELECT CEIL(AVG(t3.diff)) as avg FROM (SELECT t1.user_id,TIMESTAMPDIFF(DAY,t1.st,t2.en) as diff FROM (SELECT created_at  as st,user_id FROM `progresses` WHERE (progress_type LIKE '%Session' AND progress_id=?) AND user_id IN (" . implode(",", $users) . ")) t1
+                $query = DB::select("SELECT CEIL(AVG(t3.diff)) as avg FROM (SELECT t1.user_id,TIMESTAMPDIFF(DAY,t1.st,t2.en) as diff FROM (SELECT created_at  as st,user_id FROM `progresses` WHERE (progress_type LIKE '%Session' AND progress_id=?) AND user_id IN (" . implode(",", $users) . ")) t1
 INNER JOIN (SELECT created_at  as en,user_id FROM `progresses` WHERE (progress_type LIKE '%Lesson' AND progress_id=?) AND user_id IN (" . implode(",", $users) . ")) t2 ON t1.user_id = t2.user_id) t3", [$sessionId, $lesson->id]);
 
-            $lessonAvgCompletion[$lesson->title] = $query[0]->avg;
+                $lessonAvgCompletion[$lesson->title] = $query[0]->avg;
 
-            if ($lessonAvgCompletion[$lesson->title] == null) {
+                if ($lessonAvgCompletion[$lesson->title] == null) {
+                    $lessonAvgCompletion[$lesson->title] = 0;
+                }
+            } else {
                 $lessonAvgCompletion[$lesson->title] = 0;
             }
 
@@ -383,20 +387,25 @@ INNER JOIN (SELECT created_at  as en,user_id FROM `progresses` WHERE (progress_t
 
         foreach ($moduleM->lmsLessons as $lesson) {
 
-            $lessonCount[$lesson->title] = DB::select("select COUNT(*) as counted from progresses where progress_type LIKE '%Lesson' AND progress_id=? AND user_id IN (" . implode(",", $users) . ")", [$lesson->id]);
-            $lessonCount[$lesson->title] = $lessonCount[$lesson->title][0]->counted;
-            $lessonCount[$lesson->title] = $this->percentage($totalUsers, $lessonCount[$lesson->title]);
+            if($lesson->sessions->count() > 0) {
+                $lessonCount[$lesson->title] = DB::select("select COUNT(*) as counted from progresses where progress_type LIKE '%Lesson' AND progress_id=? AND user_id IN (" . implode(",", $users) . ")", [$lesson->id]);
+                $lessonCount[$lesson->title] = $lessonCount[$lesson->title][0]->counted;
+                $lessonCount[$lesson->title] = $this->percentage($totalUsers, $lessonCount[$lesson->title]);
 
-            $sessionId = $lesson->sessions->first()->id;
+                $sessionId = $lesson->sessions->first()->id;
 
-            $query = DB::select("SELECT CEIL(AVG(t3.diff)) as avg FROM (SELECT t1.user_id,TIMESTAMPDIFF(DAY,t1.st,t2.en) as diff FROM (SELECT created_at  as st,user_id FROM `progresses` WHERE (progress_type LIKE '%Session' AND progress_id=?) AND user_id IN (" . implode(",", $users) . ")) t1
+                $query = DB::select("SELECT CEIL(AVG(t3.diff)) as avg FROM (SELECT t1.user_id,TIMESTAMPDIFF(DAY,t1.st,t2.en) as diff FROM (SELECT created_at  as st,user_id FROM `progresses` WHERE (progress_type LIKE '%Session' AND progress_id=?) AND user_id IN (" . implode(",", $users) . ")) t1
 INNER JOIN (SELECT created_at  as en,user_id FROM `progresses` WHERE (progress_type LIKE '%Lesson' AND progress_id=?) AND user_id IN (" . implode(",", $users) . ")) t2 ON t1.user_id = t2.user_id) t3", [$sessionId, $lesson->id]);
 
-            $lessonAvgCompletion[$lesson->title] = $query[0]->avg;
+                $lessonAvgCompletion[$lesson->title] = $query[0]->avg;
 
-            if ($lessonAvgCompletion[$lesson->title] == null) {
+                if ($lessonAvgCompletion[$lesson->title] == null) {
+                    $lessonAvgCompletion[$lesson->title] = 0;
+                }
+            } else {
                 $lessonAvgCompletion[$lesson->title] = 0;
             }
+
         }
 
         foreach ($lessonL->sessions as $session) {
