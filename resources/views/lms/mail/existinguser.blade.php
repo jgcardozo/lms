6906150fda15c259d5ff818be1f9afb6d70db13f
Carlos Lizaola <carlos@askmethod.com>
2018-09-10 08:58:38 -0500

    update email copy for existing user

--- resources/views/lms/mail/existinguser.blade.php
+++ resources/views/lms/mail/existinguser.blade.php
@@ -1,6 +1,12 @@
-<p>You can login to the ASK Method training area with the link below.</p>
-
-<p><a href="{{ route('home') }}">ASK Method</a> <br>
-<strong>Your username:</strong> {{ $email }}</p>
-
-<p>You can also reset your password on that page, if necessary.</p>
+<p>To access your ASK Method content, please log in to our training portal with your current account.</p>
+<br />
+<p>1. Go to: <a href="{{ route('home') }}">https://learn.askmethod.com/</a></p>
+<p>2. Enter your email address: {{ $email }}</p>
+<p>3. Enter your current password</p>
+<p>(If you don't know your password, just click "Forgot Password?" on the page above)</p>
+<p>4. Click Sign In</p>
+<br />
+<p>All my best,</p>
+<p>Ryan :slightly_smiling_face:</p>
+<br />
+<p>P.S. If you have any trouble, feel free to reach out to our Support team: contact@askmethod.com</p>
