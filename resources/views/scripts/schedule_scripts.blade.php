de760908648bb966282ac2764f9105cf8f370152
pecko95 <petar@codeart.mk>
2020-11-10 17:05:50 +0100

    Temporarily uncommented bootstrap.min.css cdn link

--- resources/views/scripts/schedule_scripts.blade.php
+++ resources/views/scripts/schedule_scripts.blade.php
@@ -1,6 +1,6 @@
 <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 {{--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/css/bootstrap-datetimepicker.min.css">--}}
-{{--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">--}}
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
 
 <script src="{{ mix('js/app.js') }}"></script>
 {{--<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>--}}
