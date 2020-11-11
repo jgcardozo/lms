3c0e004954235eb0673e1bbb7fed6a133850490a
Deniz Jusufovic <developer@codeart.mk>
2020-11-11 14:38:48 +0100

    changes in the backend part of logs export

--- app/Http/Controllers/LogExportController.php
+++ app/Http/Controllers/LogExportController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\Admin\ESSubjectResource;
 use App\Models\Cohort;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\StreamedResponse;
 
@@ -11,93 +12,107 @@ class LogExportController extends Controller
 {
     public function __invoke(Request $request)
     {
-        $filters = [
-                "causer"=> "all",    // admin, user or "all"
-                "cohort"=> "all",        // cohort ID or "all"
-                "action"=> "all",        // action ID or "all"
-                "activity"=> "all",  // activity ID or "all"
-                "sort"=> "timestamp",       // po koja kolona se sortira
-                "order"=> "desc",     // asc or desc
-                "fromDate"=> null,   // filter From
-                "toDate"=> null,     // filter To
-                "user_id"=> null    // user ID or null
-            ];
+        $request->validate([
+            '_token' => 'required'
+        ]);
 
-//        $filters = $request->get('filters');
-
-        if($filters['user_id']) {
-            $logs = \App\Models\Log::with('user.cohorts')->with('action')->with('activity')->with('subject')->where('user_id',$filters['user_id'])->orderBy('created_at','DESC');
+        if($request->get('user_id')) {
+            $logs = \App\Models\Log::with('user.cohorts')->with('action')->with('activity')->with('subject')->where('user_id',$request->get('user_id'));
         }
         else {
-            $logs = \App\Models\Log::with('user.cohorts')->with('action')->with('activity')->with('subject')->orderBy('created_at','DESC');
+            $logs = \App\Models\Log::with('user.cohorts')->with('action')->with('activity')->with('subject');
 
-            if ($filters['causer'] === 'admin') {
+            if ($request->get('causer') === 'admin') {
                 $logs = $logs->where('activity_id','=',7);
-            } elseif ($filters['causer'] === 'user') {
+            } elseif ($request->get('causer') === 'user') {
                 $logs = $logs->where('activity_id','!=',7);
             }
 
-
-            if ($filters['action'] !== 'all') {
-                $logs = $logs->where('action_id','=',$filters['action']);
+            if ($request->get('action') !== 'all') {
+                $logs = $logs->where('action_id','=',$request->get('action'));
             }
 
-            if ($filters['activity'] !== 'all') {
-                $logs = $logs->where('activity_id','=',$filters['activity']);
+            if ($request->get('activity') !== 'all') {
+                $logs = $logs->where('activity_id','=',$request->get('activity'));
             }
 
-            if ($filters['fromDate']) {
-                $logs = $logs->where('created_at','>=', date("Y-m-d H:i:s", strtotime($filters['fromDate'])));
+            if ($request->get('fromDate')) {
+                $logs = $logs->where('created_at','>=', date("Y-m-d H:i:s", strtotime($request->get('fromDate'))));
             }
 
-            if ($filters['toDate']) {
-                $logs = $logs->where('created_at','<=', date("Y-m-d H:i:s", strtotime($filters['toDate'])));
+            if ($request->get('toDate')) {
+                $logs = $logs->where('created_at','<=', date("Y-m-d H:i:s", strtotime($request->get('toDate'))));
             }
 
-            if($filters['cohort'] !== 'all') {
-                $cohort = Cohort::find($filters['cohort']);
+            if($request->get('cohort') !== 'all') {
+                $cohort = Cohort::find($request->get('cohort'));
                 $userIds = $cohort->users()->pluck('users.id');
                 $logs = $logs->whereIn('user_id', $userIds);
             }
-
-            $logs = $logs->orderBy('created_at', 'DESC');
         }
 
-        $response = new StreamedResponse(function() use ($logs){
-            // Open output stream
-            $handle = fopen('php://output', 'w');
+        $logs = $logs->orderBy(
+            $this->getSortColumn($request),
+            $request->get('order')
+        );
 
-            // Add CSV headers
-            fputcsv($handle, [
-                'ID',
-                'User',
-                'Action',
-                'Subject',
-                'Timestamp'
+
+        try {
+            $datetime = Carbon::now()->format('Y-m-d_H-i-s');
+            $filename = "ask_lms_logs_{$datetime}.csv";
+            $response = new StreamedResponse(function() use ($logs){
+                // Open output stream
+                $handle = fopen('php://output', 'w');
+
+                // Add CSV headers
+                fputcsv($handle, [
+                    'ID',
+                    'User',
+                    'Action',
+                    'Subject',
+                    'Timestamp'
+                ]);
+
+                // Get all users
+                $logs->chunk(5000, function($chunk) use($handle) {
+                    foreach ($chunk as $log) {
+                        $subject = new ESSubjectResource($log);
+                        // Add a new row with data
+                        fputcsv($handle, [
+                            $log->id,
+                            $log->user !== null ? $log->user->name : $log->deleted_user,
+                            $log->action !== null ? $log->action->name : "",
+                            $subject->resolve()['tree'],
+                            $log->created_at
+                        ]);
+                    }
+                });
+
+                // Close the output stream
+                fclose($handle);
+            }, 200, [
+                'Content-Type' => 'text/csv',
+                'Content-Disposition' => 'attachment; filename="'. $filename .'"',
             ]);
 
-            // Get all users
-            $logs->chunk(5000, function($chunk) use($handle) {
-                foreach ($chunk as $log) {
-                    $subject = new ESSubjectResource($log);
-                    // Add a new row with data
-                    fputcsv($handle, [
-                        $log->id,
-                        $log->user !== null ? $log->user->name : $log->deleted_user,
-                        $log->action !== null ? $log->action->name : "",
-                        $subject->resolve()['tree'],
-                        $log->created_at
-                    ]);
-                }
-            });
+            return $response;
+        } catch (\Exception $e) {
+            \Illuminate\Support\Facades\Log::error("ERROR IN CSV EXPORT: Code: {$e->getCode()}; Message: {$e->getMessage()}");
+            throw $e;
+        }
+    }
 
-            // Close the output stream
-            fclose($handle);
-        }, 200, [
-            'Content-Type' => 'text/csv',
-            'Content-Disposition' => 'attachment; filename="export.csv"',
-        ]);
-
-        return $response;
+    private function getSortColumn(Request $request)
+    {
+        switch ($request->get('sort')) {
+            case "id":
+                return "id";
+            case "user":
+                return "user.email";
+            case "action":
+                return "action.name";
+            default:
+                return "created_at";
+        }
     }
 }
